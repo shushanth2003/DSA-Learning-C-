8c26c5641088816c77077e5342e3b1a935c2e26a
shushanth2003 <bsshu2003@gmail.com>
2026-07-13 17:17:25 +0530

    Feat: Add a initialization:

---
+++ Arrays/Arrays/RotatedArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class RotatedArray
+    {
+        public int[] nums;
+        public int k;
+        public RotatedArray(int[] arr,int k)
+        {
+            this.nums = arr;
+            this.k = k;
+        }
+
+        public void setRotateArray()
+        {
+            int n = nums.Length;
+            reverseRotate(nums, 0, n-1);
+            reverseRotate(nums, k, n - 1);
+            reverseRotate(nums, 0, k);
+        }
+
+        public void reverseRotate(int[] nums, int start,int end)
+        {
+            while(start<=end)
+            {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
